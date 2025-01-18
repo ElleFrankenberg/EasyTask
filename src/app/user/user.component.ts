e53a4d3836d0157790dfab02356dc72f85ec004d
Elle Frankenberg <eleonor.frankenberg@gmail.com>
2025-01-18 11:39:06 +0100

    project Easytask

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,45 @@
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  input,
+  Output,
+  output,
+} from '@angular/core';
+import { type User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
+  // another/modern way of writing the same output thing
+  // select = output<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  // signals for properties
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
